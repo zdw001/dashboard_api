1c9fd37f9754eec11e0e7a09916ece5d17d8eee9
zach <zachwinters1@gmail.com>
2023-02-25 14:14:47 -0800

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Gun = require('gun')
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8080;
 app.use(Gun.serve)
 
 app.get('/', (req, res) => {
